65d7fe8e51c745b55b9323b82d87aebdb65dbfe5
ListerTaboo <death2018bringer@gmail.com>
2025-10-01 23:41:15 +0400

    second commit

---
+++ Level 02/Classwork/Main.py
@@ -0,0 +1,5 @@
+print(5 + 65)
+print(12 - 5)
+
+print("Ilia" + "Tabatadze")
+print("")
---
+++ Level 02/Classwork/Task 1.py
@@ -0,0 +1,9 @@
+"""1) შექმენით ცვლადი სახელად full_name რომელშიც შეინახავთ სრულ სახელს ტექსტურად, შემდეგ კი დაბეჭდეთ ცვლადი"""
+
+Full_name = " Ilia Tabatadzee "
+year = 2025
+
+print( 5 )
+print(" Taboo is the best  ! ")
+print(Full_name)
+print(year)
