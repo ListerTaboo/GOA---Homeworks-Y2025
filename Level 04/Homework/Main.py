3e10efabfe8d007c3e9d1b2d46346d3bbdba1887
ListerTaboo <death2018bringer@gmail.com>
2025-10-07 18:07:44 +0400

    fourth commit\

---
+++ Level 04/Homework/Main.py
@@ -0,0 +1,40 @@
+Mom =  "Katie"
+Dad = "Shalva"
+Brother = "Anton"
+Sister = "Elene"
+Brother_2 = "Andria"
+
+print(Mom)
+print(Dad)
+print(Brother)
+print(Sister)
+print(Brother_2)
+
+print(" my " + " mom " + " is " + " Beautiful ")
+
+Number_1 = 5
+Number_2 = 3
+Number_3 = 8
+Number_4 = 9
+Number_5 = 10
+
+print(Number_1 + Number_2)
+print(Number_3 * Number_1)
+print(Number_5 / Number_4)
+print(Number_3 - Number_1)
+
+
+#debugger
+
+balance = 100
+continent = "Europa"
+reward = 50
+
+print(type(balance))
+print(type(continent))
+print(type(reward))
+
+print("ჯამი:", balance + reward)
+print("სხვაობა:", balance - reward)
+print("ნამრავლი:", balance * reward)
+print("განაყოფი:", balance / reward)
